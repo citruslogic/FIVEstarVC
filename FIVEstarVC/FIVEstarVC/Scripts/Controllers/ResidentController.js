f19a73b2a7be9f8f228224dde97d4f92c8dbc032
citruslogic <phranq@gmail.com>
2017-10-07 00:59:21 -0400

    Add ResidentController.js

---
+++ FIVEstarVC/FIVEstarVC/Scripts/Controllers/ResidentController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('fivestarApp')
+        .controller('ResidentController', ResidentController);
+
+
+    $.scope.arrivaldate = new Date();
+    $.scope.departdate = new Date();
+
+    controller.$inject = ['$scope'];
+
+    function controller($scope) {
+        $scope.title = 'ResidentController';
+
+        activate();
+
+        function activate() { }
+    }
+
+
+})();
