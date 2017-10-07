ea022bdd20c640ffbf07c77cd9562bdd7c5226f0
citruslogic <phranq@gmail.com>
2017-10-07 14:10:12 -0400

    added modal form.

---
+++ FIVEstarVC/FIVEstarVC/Scripts/app/app.js
@@ -0,0 +1,44 @@
+﻿var app = angular.module('FiveStarApp', ['ui.bootstrap']);
+    app.controller('ResidentController', ['$scope', '$modal', 
+       
+        function ($scope, $modal) {
+
+       // $scope.answer = function () {
+       //     return $scope.correctAnswer ? 'correct' : 'incorrect';
+       // };
+   
+
+            $scope.showForm = function () {
+                $scope.message = "Show Form Button Clicked";
+
+                var modalInstance = $modal.open({
+                    templateUrl: 'modal-form.html',
+                    controller: ModalInstanceCtrl,
+                    scope: $scope,
+                    resolve: {
+                        userForm: function () {
+                            return $scope.userForm;
+                        }
+                    }
+                });
+
+
+            };
+
+            var ModalInstanceCtrl = function ($scope, $modalInstance, userForm) {
+                $scope.form = {}
+                $scope.submitForm = function () {
+                    if ($scope.form.userForm.$valid) {
+                        $modalInstance.close('closed');
+
+                    };
+
+                    $scope.cancel = function () {
+                        $modalInstance.dismiss('cancel');
+                    };
+
+
+
+                };
+            };
+  
