c781aa5a13af9f679fcb69b02a1a796e41546c09
citruslogic <phranq@gmail.com>
2017-10-20 12:33:10 -0400

    Simple user registration support added.

---
+++ FIVEstarVC/FIVEstarVC/Startup.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(FIVEstarVC.Startup))]
+
+namespace FIVEstarVC
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Login")
+            });
+        }
+    }
+}
