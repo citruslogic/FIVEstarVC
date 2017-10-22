9574e4c2a3239e20f553ccfb876e6a9bb6235070
citruslogic <phranq@gmail.com>
2017-10-22 00:10:08 -0400

    Accounts revision. Slightly better authorization.

--- FIVEstarVC/FIVEstarVC/App_Start/Startup.Auth.cs
+++ FIVEstarVC/FIVEstarVC/App_Start/Startup.Auth.cs
@@ -1,22 +1,41 @@
-﻿using Microsoft.AspNet.Identity;
+﻿using System.Web;
+using System.Web.Mvc;
+
+using Microsoft.AspNet.Identity;
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
 using Owin;
 
-[assembly: OwinStartup(typeof(FIVEstarVC.Startup))]
-
 namespace FIVEstarVC
 {
-    public class Startup
+    public partial class Startup
     {
-        public void Configuration(IAppBuilder app)
+        // For more information on configuring authentication, please visit http://go.microsoft.com/fwlink/?LinkId=301864
+        public void ConfigureAuth(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            // Enable the application to use a cookie to store information for the signed in user
             app.UseCookieAuthentication(new CookieAuthenticationOptions
             {
                 AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Login")
+                LoginPath = new PathString("/Account/Login")
             });
+            // Use a cookie to temporarily store information about a user logging in with a third party login provider
+            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Uncomment the following lines to enable logging in with third party login providers
+            //app.UseMicrosoftAccountAuthentication(
+            // clientId: "",
+            // clientSecret: "");
+
+            //app.UseTwitterAuthentication(
+            // consumerKey: "",
+            // consumerSecret: "");
+
+            //app.UseFacebookAuthentication(
+            // appId: "",
+            // appSecret: "");
+
+            //app.UseGoogleAuthentication();
         }
     }
 }
---
+++ FIVEstarVC/FIVEstarVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FIVEstarVC.Startup))]
+namespace FIVEstarVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        } 
+    }
+}
