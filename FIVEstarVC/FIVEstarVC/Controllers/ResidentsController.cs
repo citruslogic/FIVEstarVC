5c9093bf34853a40dc7df7b1c6c736dbada2de70
citruslogic <phranq@gmail.com>
2017-10-21 23:51:21 -0400

    Residents management, simple auth

---
+++ FIVEstarVC/FIVEstarVC/Controllers/ResidentsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FIVEstarVC.Models;
+
+namespace FIVEstarVC.Controllers
+{
+    public class ResidentsController : Controller
+    {
+        private FiveStarModel db = new FiveStarModel();
+
+        // GET: Residents
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Residents.ToListAsync());
+        }
+
+        // GET: Residents/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resident resident = await db.Residents.FindAsync(id);
+            if (resident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resident);
+        }
+
+        // GET: Residents/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Residents/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ResidentID,LastName,FirstName,RoomNumber")] Resident resident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Residents.Add(resident);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(resident);
+        }
+
+        // GET: Residents/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resident resident = await db.Residents.FindAsync(id);
+            if (resident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resident);
+        }
+
+        // POST: Residents/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ResidentID,LastName,FirstName,RoomNumber")] Resident resident)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resident).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(resident);
+        }
+
+        // GET: Residents/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resident resident = await db.Residents.FindAsync(id);
+            if (resident == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resident);
+        }
+
+        // POST: Residents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Resident resident = await db.Residents.FindAsync(id);
+            db.Residents.Remove(resident);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
