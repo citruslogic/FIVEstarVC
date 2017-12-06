28f589d583425abc69302770ba6e4a2d5441c138
Frank Butler <phranq@gmail.com>
2017-12-06 12:34:09 -0500

    Added dashboard.

--- FIVESTARVC/Controllers/HomeController.cs
+++ FIVESTARVC/Controllers/HomeController.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using FIVESTARVC.Models;
+using FIVESTARVC.DAL;
 
 namespace FIVESTARVC.Controllers
 {
+
     public class HomeController : Controller
     {
+        private ResidentContext db = new ResidentContext();
+
         public ActionResult Index()
         {
+            ViewBag.lastFiveResidents = db.Residents.OrderByDescending(r => r.LastName).Take(5);
+
             return View();
         }
 
