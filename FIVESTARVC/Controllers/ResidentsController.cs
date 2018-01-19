02998d3c4db6019b30b0a63811aeca45f09e83fd
David Thompson <dthompson1413@gmail.com>
2018-01-18 16:28:16 -0800

    Commit for displaying the room file on the crate Resident page.  Having trouble getting it narrowed down to just the occupied rooms.

--- FIVESTARVC/Controllers/ResidentsController.cs
+++ FIVESTARVC/Controllers/ResidentsController.cs
@@ -94,6 +94,7 @@ namespace FIVESTARVC.Controllers
         // GET: Residents/Create
         public ActionResult Create()
         {
+            //ViewBag.AvailRoom = db.Rooms.Contains;
 
             //Call Get Assigned Room to get available rooms//
 
