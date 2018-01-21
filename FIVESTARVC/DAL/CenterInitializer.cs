30a312bec349de34271b5836116787613d3bdd65
David Thompson <dthompson1413@gmail.com>
2018-01-20 16:34:28 -0800

    Pushing to Origin AddRoom so Frank can look at it

--- FIVESTARVC/DAL/CenterInitializer.cs
+++ FIVESTARVC/DAL/CenterInitializer.cs
@@ -13,9 +13,9 @@ namespace FIVESTARVC.DAL
         {
             var residents = new List<Resident>
             {
-                new Resident { FirstMidName="Carson", LastName="Steven", ServiceBranch=ServiceType.ARMY, Room=100 },
-                new Resident { FirstMidName="Naomi", LastName="Wildman", ServiceBranch=ServiceType.AIRFORCE, Room=200 },
-                new Resident { FirstMidName="Gary", LastName="Noonan", ServiceBranch=ServiceType.NAVY, Room =110 }
+                new Resident { FirstMidName="Carson", LastName="Steven", ServiceBranch=ServiceType.ARMY, Room=102},
+                new Resident { FirstMidName="Naomi", LastName="Wildman", ServiceBranch=ServiceType.AIRFORCE, Room=103 },
+                new Resident { FirstMidName="Gary", LastName="Noonan", ServiceBranch=ServiceType.NAVY, Room =105 }
             };
 
             residents.ForEach(r => context.Residents.Add(r));
@@ -40,6 +40,61 @@ namespace FIVESTARVC.DAL
 
             militaryCampaigns.ForEach(m => context.MilitaryCampaigns.Add(m));
             context.SaveChanges();
+
+            var Rooms = new List<Room>
+            {
+                //Rooms on the E/S Wing//
+
+               new Room { RoomNum=102, ResidentID = 1},
+               new Room { RoomNum=103, ResidentID = 2},
+               new Room { RoomNum=105, ResidentID = 3},
+               new Room { RoomNum=106},
+               new Room { RoomNum=107},
+               new Room { RoomNum=108},
+               new Room { RoomNum=109},
+               new Room { RoomNum=110},
+               new Room { RoomNum=112},
+               new Room { RoomNum=114},
+               new Room { RoomNum=115},
+               new Room { RoomNum=116},
+               new Room { RoomNum=117},
+               new Room { RoomNum=118},
+               new Room { RoomNum=119},
+               new Room { RoomNum=120},
+               new Room { RoomNum=121},
+               new Room { RoomNum=122},
+               new Room { RoomNum=123},
+               new Room { RoomNum=124},
+               new Room { RoomNum=125},
+               new Room { RoomNum=126},
+
+               //Rooms on the West Wing//
+
+               new Room { RoomNum=202},
+               new Room { RoomNum=203},
+               new Room { RoomNum=204},
+               new Room { RoomNum=205},
+               new Room { RoomNum=206},
+               new Room { RoomNum=207},
+               new Room { RoomNum=208},
+               new Room { RoomNum=209},
+
+               //Rooms on the North Wing//
+
+               new Room { RoomNum=301},
+               new Room { RoomNum=303},
+               new Room { RoomNum=304},
+               new Room { RoomNum=305},
+               new Room { RoomNum=306},
+               new Room { RoomNum=307},
+               new Room { RoomNum=308},
+               new Room { RoomNum=310},
+
+            };
+
+            Rooms.ForEach(m => context.Rooms.Add(m));
+            context.SaveChanges();
+
         }
     }
 }
--- FIVESTARVC/Models/Room.cs
+++ FIVESTARVC/Models/Room.cs
@@ -9,12 +9,10 @@ using System.Web;
 namespace FIVESTARVC.Models
 {
     public class Room
-    { 
-        
+    {
+        internal int ResidentID;
 
-      
-               
-        [Key, ForeignKey ("Resident")]
+        [Key, ForeignKey("Resident")]
         public int RoomID { get; set; }
 
         [Display(Name = "Room Number")]
@@ -22,11 +20,10 @@ namespace FIVESTARVC.Models
 
         [Display(Name = "Is Occupied")]
         public bool IsOccupied { get; set; }
-
+        public bool IsSelected { get; set;
+        }
         public virtual Resident Resident { get; set; }
 
-       
-        public virtual ICollection<Resident> GetRoom { get; set; }
 
         public static implicit operator Room(int v)
         {
