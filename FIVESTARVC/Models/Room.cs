563b3953caa8b2d281dea543e32ffee14dc3c3b7
David Thompson <dthompson1413@gmail.com>
2018-01-22 20:45:04 -0800

    Can't get the room numbers to print out, but the code compiles

--- FIVESTARVC/Models/Room.cs
+++ FIVESTARVC/Models/Room.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace FIVESTARVC.Models
 {
-    public class Room
+    public class Room : IEnumerable
     {
+
+        public int RoomID { get; set; }
+        public int? ResidentID { get; set; }
+        public int RoomNum { get; set; }
+        public bool IsOccupied { get; set; }
+
+
+        public virtual ICollection<Room> Rooms { get; set; }
+
+        public IEnumerator GetEnumerator()
+        {
+            return ((IEnumerable)Rooms).GetEnumerator();
+        }
     }
 }
