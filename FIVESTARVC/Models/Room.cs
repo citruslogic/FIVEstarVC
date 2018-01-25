cdeaf6da6ba80e15bcb88b0a04752edc90bdf9ef
David Thompson <dthompson1413@gmail.com>
2018-01-24 20:28:10 -0800

    Added Available Rooms to the Create Get method

---
+++ FIVESTARVC/Models/Room.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FIVESTARVC.Models
+{
+    public class Room
+    {
+               
+        public int RoomID { get; set; }
+
+       public int? ResidentID { get; set; }
+
+        [Display(Name = "Room Number")]
+        public int RoomNum { get; set; }
+
+        [Display(Name = "Is Occupied")]
+        public bool IsOccupied { get; set; }
+                
+    }
+}
