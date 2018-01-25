383418c5b8a16c6d95f00a1e30a85db004c6a087
David Thompson <dthompson1413@gmail.com>
2018-01-24 20:28:10 -0800

    Added Available Rooms to the Create Get method

---
+++ FIVESTARVC/ViewModels/AssignedRoom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FIVESTARVC.ViewModels
+{
+    public class AssignedRoom
+    {
+       
+        public int RoomNum { get; set; }
+
+        
+        public bool IsOccupied { get; set; }
+
+    }
+}
