b338aeeaec926c696f67ab25e03652c13e8a3a45
Frank Butler <n00184117@ospreys.unf.edu>
2018-02-02 10:03:14 -0500

    Found the '>'

--- FIVESTARVC/Models/Resident.cs
+++ FIVESTARVC/Models/Resident.cs
@@ -47,7 +47,7 @@ namespace FIVESTARVC.Models
         public DateTime? Birthdate { get; set; }
         [Display(Name = "Service Branch")]
         public ServiceType ServiceBranch { get; set; }
-        [Display(Name = "PTSD")]
+        [Display(Name = "Resident has PTSD?")]
         public Boolean HasPTSD { get; set; }
         [Display(Name = "In Veterans Court")]
         public Boolean InVetCourt { get; set; }
