0c67860c30e7d423b370b338fff66d559131abc2
Frank Butler <phranq@gmail.com>
2018-02-08 16:14:45 -0500

    Resident model missing fields.

---
+++ FIVESTARVC/ViewModels/ResidentIncomeModel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FIVESTARVC.Models;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIVESTARVC.ViewModels
+{
+    public class ResidentIncomeModel
+    {
+        
+            // RESIDENT
+            public int ResidentID { get; set; }
+            [Required]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            [Display(Name = "First Name")]
+            public string FirstMidName { get; set; }
+            [Display(Name = "Birthdate")]
+            [DataType(DataType.Date)]
+            [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+            public DateTime? Birthdate { get; set; }
+            [Display(Name = "Service Branch")]
+            public ServiceType ServiceBranch { get; set; }
+            [Display(Name = "Resident has PTSD?")]
+            public Boolean HasPTSD { get; set; }
+            [Display(Name = "In Veterans Court")]
+            public Boolean InVetCourt { get; set; }
+            [Display(Name = "Room Number")]
+            public int RoomID { get; set; }
+
+        // BENEFIT
+        public int BenefitID { get; set; }
+
+        [Display(Name = "Disability Rating (%)")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:P2}")]
+        public double? DisabilityPercentage { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Total Benefit Amount")]
+        public decimal TotalBenefitAmount { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        public decimal? SSI { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        public decimal? SSDI { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Food stamps")]
+        public decimal? FoodStamp { get; set; }
+
+        /* Other forms of disability as income */
+        [Display(Name = "Other Income")]
+        public String OtherDescription { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [Display(Name = "Other (Amount)")]
+        public decimal? Other { get; set; }
+        /***************************************/
+    }
+}
