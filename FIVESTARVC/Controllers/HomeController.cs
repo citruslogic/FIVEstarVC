97ece0a2fa31b8f087d59e08b77f4e79dc5c1f9c
Frank Butler <n00184117@ospreys.unf.edu>
2018-03-03 10:53:18 -0500

    Upcoming birthdays feature added.

--- FIVESTARVC/Controllers/HomeController.cs
+++ FIVESTARVC/Controllers/HomeController.cs
@@ -15,6 +15,8 @@ namespace FIVESTARVC.Controllers
     public class HomeController : Controller
     {
         private ResidentContext db = new ResidentContext();
+        public IEnumerable<Resident> NearestResidents { get; set; }           // Nearest birthdays.
+
 
         public ActionResult Index()
         {
@@ -40,7 +42,7 @@ namespace FIVESTARVC.Controllers
             int roomsMax = db.Rooms.Count();
             ViewBag.roomsMax = roomsMax;
 
-            ViewBag.PercentFilled = ((double) roomsOccupied / (double) roomsMax) * 100;
+            ViewBag.PercentFilled = roomsOccupied / (double)roomsMax * 100;
 
             /* David Thompson's (dthompson) grad count */
             //Finds graduation percent
@@ -50,12 +52,34 @@ namespace FIVESTARVC.Controllers
             //Finds number admitted
             double Admitted = db.ProgramEvents.Count(x => x.ProgramTypeID == 2);
             ViewBag.Admitted = Admitted;
+            ViewBag.gradPercent = Graduated / (double)Admitted * 100;
 
-            ViewBag.gradPercent = ((double) Graduated / (double) Admitted) * 100;
+
+            /*******************************************/
+
+            /* Get the resident with the nearest birthday */
+            FindNearest();
+            ViewBag.NearestResidents = NearestResidents;
 
             return View(residents.ToList());
         }
 
-       
+        /* Get the resident with the nearest birthday. */
+        /* https://www.ict.social/csharp/wpf/course-birthday-reminder-in-csharp-net-wpf-logic-layer */
+        private void FindNearest()
+        {
+            var sortedResidents = db.Residents.ToList().OrderBy(o => o.RemainingDays);
+
+            if (sortedResidents.Count() > 0)
+                NearestResidents = sortedResidents.Take(2);
+            else
+                NearestResidents = null;
+        }
+
+
+
+
+
     }
 }
+ 
