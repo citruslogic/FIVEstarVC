6ff91b147604c09dee3b7e3f8e7ebbbee4629099
Frank Butler <n00184117@ospreys.unf.edu>
2018-03-03 08:28:45 -0500

    Progress bars added.

--- FIVESTARVC/Controllers/HomeController.cs
+++ FIVESTARVC/Controllers/HomeController.cs
@@ -32,7 +32,26 @@ namespace FIVESTARVC.Controllers
                      }).OrderByDescending(r => r.ResidentID).Take(5);
 
             ViewBag.pop = db.Residents.ToList().Where(r => r.IsCurrent()).Count();
-            ViewBag.roomCap = db.Rooms.Where(rm => rm.IsOccupied == false).Count() + " / " + db.Rooms.Count();
+            ViewBag.allpop = db.Residents.ToList().Count();
+
+            int roomsOccupied = db.Rooms.Where(rm => rm.IsOccupied == false).Count();
+            ViewBag.roomsOccupied = roomsOccupied;
+
+            int roomsMax = db.Rooms.Count();
+            ViewBag.roomsMax = roomsMax;
+
+            ViewBag.PercentFilled = ((double) roomsOccupied / (double) roomsMax) * 100;
+
+            /* David Thompson's (dthompson) grad count */
+            //Finds graduation percent
+            double Graduated = db.ProgramEvents.Count(x => x.ProgramTypeID == 4);
+            ViewBag.Graduated = Graduated;
+
+            //Finds number admitted
+            double Admitted = db.ProgramEvents.Count(x => x.ProgramTypeID == 2);
+            ViewBag.Admitted = Admitted;
+
+            ViewBag.gradPercent = ((double) Graduated / (double) Admitted) * 100;
 
             return View(residents.ToList());
         }
