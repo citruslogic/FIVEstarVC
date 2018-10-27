9f492a111d7faa391319f601d94f76c3850e0e71
Frank Butler <n00184117@ospreys.unf.edu>
2018-10-27 19:58:31 -0400

    Discharge form buttons moved to right. Room management buttons revised. Compression enabled.

--- FIVESTARVC/Global.asax.cs
+++ FIVESTARVC/Global.asax.cs
@@ -16,6 +16,9 @@ namespace FIVESTARVC
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
